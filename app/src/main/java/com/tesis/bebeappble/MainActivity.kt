d923a27b9bf4dadb1a5d5ae54eaac6ef1a14dddf
Hamilton Urbano <haurbano.dev@gmail.com>
2020-11-22 16:23:18 -0500

    Adding view and listeners

--- app/src/main/java/com/tesis/bebeappble/MainActivity.kt
+++ app/src/main/java/com/tesis/bebeappble/MainActivity.kt
@@ -1,17 +1,39 @@
 package com.tesis.bebeappble
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import com.tesis.bebeappble.bluetooth.BluetoothCommunication
-import com.tesis.bebeappble.common.Message
+
 import com.tesis.bebeappble.bluetooth.TAG
+import com.tesis.bebeappble.common.Message
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextMessage: EditText
+    private lateinit var btnSendMessage: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        retrieveViews()
+        addListeners()
         BluetoothCommunication.startBLE(this)
+    }
+
+    private fun retrieveViews() {
+        btnSendMessage = findViewById(R.id.btnSendMessaje)
+        editTextMessage = findViewById(R.id.editTextMsj)
+    }
+
+    private fun addListeners() {
+        btnSendMessage.setOnClickListener {
+            val msj = editTextMessage.text.toString()
+            BluetoothCommunication.sendMessage(msj)
+        }
     }
 
     override fun onStart() {
